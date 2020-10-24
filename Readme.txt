616d1960ab598b9a070698306ac91bd23429938a
Wilson Ludeña <70303021+saul-donas17@users.noreply.github.com>
2020-10-23 19:04:52 -0500

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+En este carpeta encontrarás el mismo diseño de página pero 
+con algunas diferencias a las demás proyectos,tales como el color de fondo, imagen y
+banner.
---
+++ assets/js/miJs.js
@@ -0,0 +1,7 @@
+function Animar(){
+    var bigText = document.getElementById('h1');
+    if(bigText.style.color = "#ff0066"){
+        bigText.style.color ="black"
+        bigText.style.letterSpacing ="40px"
+    }
+}
